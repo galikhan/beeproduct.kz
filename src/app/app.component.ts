fd2ef4d91d0605f398857788ed8bceb15da0d6b6
Галихан Хамитов <galikhan.khamitov@zee.kz>
2021-01-23 13:26:02 +0600

    refactor landing page with beeproduct data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,9 @@ import 'rxjs/add/operator/filter';
 import { DOCUMENT } from '@angular/common';
 import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
 
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = 0;
+let lastScrollTop = 0;
+const delta = 5;
+const navbarHeight = 0;
 
 @Component({
     selector: 'app-root',
@@ -18,20 +17,26 @@ var navbarHeight = 0;
 export class AppComponent implements OnInit {
     private _router: Subscription;
 
-    constructor( private renderer : Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+    constructor(
+        private renderer: Renderer2,
+        private router: Router,
+        @Inject(DOCUMENT) private document: any,
+        private element: ElementRef,
+        public location: Location) { }
     @HostListener('window:scroll', ['$event'])
     hasScrolled() {
 
-        var st = window.pageYOffset;
+        const st = window.pageYOffset;
         // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
+        if (Math.abs(lastScrollTop - st) <= delta) {
             return;
+        }
 
-        var navbar = document.getElementsByTagName('nav')[0];
+        const navbar = document.getElementsByTagName('nav')[0];
 
         // If they scrolled down and are past the navbar, add class .headroom--unpinned.
         // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
+        if (st > lastScrollTop && st > navbarHeight) {
             // Scroll Down
             if (navbar.classList.contains('headroom--pinned')) {
                 navbar.classList.remove('headroom--pinned');
@@ -41,7 +46,7 @@ export class AppComponent implements OnInit {
         } else {
             // Scroll Up
             //  $(window).height()
-            if(st + window.innerHeight < document.body.scrollHeight) {
+            if (st + window.innerHeight < document.body.scrollHeight) {
                 // $('.navbar.headroom--unpinned').removeClass('headroom--unpinned').addClass('headroom--pinned');
                 if (navbar.classList.contains('headroom--unpinned')) {
                     navbar.classList.remove('headroom--unpinned');
@@ -51,26 +56,28 @@ export class AppComponent implements OnInit {
         }
 
         lastScrollTop = st;
-    };
+    }
+
     ngOnInit() {
-      var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
-      this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
-          if (window.outerWidth > 991) {
-              window.document.children[0].scrollTop = 0;
-          }else{
-              window.document.activeElement.scrollTop = 0;
-          }
-          this.renderer.listen('window', 'scroll', (event) => {
-              const number = window.scrollY;
-              if (number > 150 || window.pageYOffset > 150) {
-                  // add logic
-                  navbar.classList.add('headroom--not-top');
-              } else {
-                  // remove logic
-                  navbar.classList.remove('headroom--not-top');
-              }
-          });
-      });
-      this.hasScrolled();
+
+        const navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
+        this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
+            if (window.outerWidth > 991) {
+                window.document.children[0].scrollTop = 0;
+            } else {
+                window.document.activeElement.scrollTop = 0;
+            }
+            this.renderer.listen('window', 'scroll', () => {
+                const number = window.scrollY;
+                if (number > 150 || window.pageYOffset > 150) {
+                    // add logic
+                    navbar.classList.add('headroom--not-top');
+                } else {
+                    // remove logic
+                    navbar.classList.remove('headroom--not-top');
+                }
+            });
+        });
+        this.hasScrolled();
     }
 }
